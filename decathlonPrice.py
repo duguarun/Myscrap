9dc9c2ac00fd2225d62d34a461f8de65c69882e4
duguarun <33994910+duguarun@users.noreply.github.com>
2018-06-14 00:03:06 +0530

    Update decathlonPrice.py

--- decathlonPrice.py
+++ decathlonPrice.py
@@ -12,15 +12,17 @@ Manufac=[]
 for j in products:
  oneby=j.find('div', {'class' : "box-product"})
  price.append(oneby.find('div', {'class':'sticker-price sticker-price--normal'}).get_text())
-for i in products:
+for i in products: #Getting Bike Model
  BikeModel.append(i.find('div', {'class':'box-product__footer'}).get_text())
-for n in products:
+for n in products: #Getting Bike Brand
  Manufac.append(n.find('span',{'class': 'manufacturer'}).get_text())
 n=len(price)
+#Printing Price and Brand together
 #for i in range(n):
 # print ('\n price:{0}, 	 BikeModel:{1},	 	Manufac:{2}'.format(price[i],BikeModel[i],Manufac[i]))
 #dictio=dict(zip(price,BikeModel))
 #print(dictio)
+#Removing Rs. Symbol from Price to save as json format
 l=['₹']
 Price = [ x.replace(y,'')  for x in price for y in l if y in x ]
 for i in range(n):
